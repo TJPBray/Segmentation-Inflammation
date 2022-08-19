8dd70326f181e42973c8ed4d396383edc4f2ecfd
timbray <timbray@rother.cs.ucl.ac.uk>
2022-08-19 15:35:20 +0100

    Include assessment of perfromance on training data and image augmentation

--- Demo scripts/inflammationDemo_v1a.m
+++ Demo scripts/inflammationDemo_v1a.m
@@ -4,22 +4,22 @@
 
 
 
-%% 5. Augment the data
+% 5. Augment the data
 
-% %5.1 Create data augmenter object (specifying specific augmentations)
-% % imageAugmenter = imageDataAugmenter( ...  % Dummy augmenter to ensure
-% % that new datastore functions as expected
-% %     'RandRotation',[0,0])
-% 
-% imageAugmenter = imageDataAugmenter( ...
-%     'RandRotation',[-20,20], ...
-%     'RandXTranslation',[-3 3], ...
-%     'RandYTranslation',[-3 3])
-% 
-% %5.2 Perform data augmentation for combined data store
-% auds = transform(ds, @(data)augment(imageAugmenter,data));
-% 
-% %5.3 Preview augmentation
+%5.1 Create data augmenter object (specifying specific augmentations)
+% imageAugmenter = imageDataAugmenter( ...  % Dummy augmenter to ensure
+% that new datastore functions as expected
+%     'RandRotation',[0,0])
+
+imageAugmenter = imageDataAugmenter( ...
+    'RandRotation',[-20,20], ...
+    'RandXTranslation',[-3 3], ...
+    'RandYTranslation',[-3 3])
+
+%5.2 Perform data augmentation for combined data store
+auTrainingDS = transform(trainingDS, @(data)augment(imageAugmenter,data));
+
+%5.3 Preview augmentation
 
 
 %% 6. Create the U-Net
@@ -37,9 +37,9 @@ plot(lgraph)
 %% 7. Train the U-net
 %7.1 Set training options (descent method, learning rate, epochs etc)
 options = trainingOptions('sgdm', ...
-    'InitialLearnRate',1e-3, ...
+    'InitialLearnRate',e-2, ...
     'MaxEpochs',20, ...
-    'MiniBatchSize', 10, ...
+    'MiniBatchSize', 20, ...
     'Plots','training-progress',...
     'VerboseFrequency',10);
 
@@ -47,26 +47,45 @@ options = trainingOptions('sgdm', ...
 % https://github.com/hongweilibran/wmh_ibbmTum - look for 
 
 %7.2 Train the network on the  dataset
-net = trainNetwork(trainingDS,lgraph,options)
+net = trainNetwork(auTrainingDS,lgraph,options)
 
 
 %% 7. Assess performance on training data
 
+% prediction = net.predict(testDS);
+prediction = net.predict(imds);
+
+i=40;
+
+img = readimage(imds,i);
+labelimage = uint16(readimage(labelds,i));
+
+figure
+title('Training dataset performance')
+subplot(2,2,1)
+imshow(img,[])
+title('Image')
+
+subplot(2,2,2)
+imshow(labelimage,[])
+title('Label')
+
+subplot(2,2,3)
+imshow(prediction(:,:,1,i),[])
+title('Prediction')
 
 
 
 %% 8. Predict
 
 %8.1 Visualise data, labels and prediction for chosen i
-i=200;
-img = testImages(:,:,i);
-labelImage = testLabels(:,:,i);
-
 % prediction = net.predict(testDS);
-prediction = net.predict(img);
+prediction = net.predict(testImds);
 
-% img = readimage(testImds,i);
-% labelimage = readimage(testLabelDS,i);
+i=28;
+
+img = readimage(testImds,i);
+labelimage = uint16(readimage(testLabelds,i));
 
 figure
 subplot(2,2,1)
@@ -74,11 +93,12 @@ imshow(img,[])
 title('Image')
 
 subplot(2,2,2)
-imshow(labelImage,[0 1])
+imshow(labelimage,[])
 title('Label')
 
 subplot(2,2,3)
-imshow(prediction(:,:,1),[0 1])
+imshow(prediction(:,:,1,i),[])
 title('Prediction')
+
 
 %8.3 Evaluate loss on test dataset matlab (calculate Dice)
