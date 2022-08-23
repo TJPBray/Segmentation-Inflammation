6db96774a3403b006a74b3e3352f51fb8c147db5
timbray <timbray@rother.cs.ucl.ac.uk>
2022-08-23 08:58:24 +0100

    Modifications for remote machine

--- Demo scripts/inflammationDemo_v1a.m
+++ Demo scripts/inflammationDemo_v1a.m
@@ -2,19 +2,17 @@
 
 %Run script following importInflammationData
 
-
-
 % 5. Augment the data
 
 %5.1 Create data augmenter object (specifying specific augmentations)
 % imageAugmenter = imageDataAugmenter( ...  % Dummy augmenter to ensure
 % that new datastore functions as expected
 %     'RandRotation',[0,0])
-
-imageAugmenter = imageDataAugmenter( ...
-    'RandRotation',[-10,10], ...
-    'RandXTranslation',[-30 30], ...
-    'RandYTranslation',[-30 30])
+% 
+% imageAugmenter = imageDataAugmenter( ...
+%     'RandRotation',[-10,10], ...
+%     'RandXTranslation',[-30 30], ...
+%     'RandYTranslation',[-30 30])
 
 
 % % %5.2 Perform data augmentation for combined data store
@@ -25,9 +23,9 @@ imageAugmenter = imageDataAugmenter( ...
 % auTrainingDsPreview = transform(imds,@classificationAugmentationPipeline, ...
 %     IncludeInfo=true);
 
-auTrainingDS = transform(trainingDS, @(data)augment(imageAugmenter,data));
+% auTrainingDS = transform(trainingDS, @(data)augment(imageAugmenter,data));
 
-auTrainingDS2 = transform(trainingDS,@segmentationAugmentationPipeline);
+auTrainingDS = transform(trainingDS,@segmentationAugmentationPipeline);
 
 %5.3 Preview augmentation
 figure
@@ -35,12 +33,12 @@ subplot(1,3,1)
 prev=preview(trainingDS);
 montage(prev{1},'DisplayRange',[])
 
-subplot(1,3,2)
-prev2=preview(auTrainingDS);
-montage(prev2{1},'DisplayRange',[])
+% subplot(1,3,2)
+% prev2=preview(auTrainingDS);
+% montage(prev2{1},'DisplayRange',[])
 
 subplot(1,3,3)
-prev3=preview(auTrainingDS2);
+prev3=preview(auTrainingDS);
 montage(prev3{1},'DisplayRange',[0 0.5])
 
 
@@ -49,18 +47,18 @@ montage(prev3{1},'DisplayRange',[0 0.5])
 %6.1 Create
 imageSize = size(trainingImages,1:2);
 numClasses = 2;
-encoderDepth=4; %Can have a deeper network than with MNIST to reflect larger images
-lgraph = unetLayers(imageSize, numClasses,'EncoderDepth',encoderDepth)
+encoderDepth=5; %Can have a deeper network than with MNIST to reflect larger images
+lgraph = unetLayersWithDiceLoss(imageSize, numClasses,'EncoderDepth',encoderDepth)
 % lgraph = unetLayersWithDiceLoss(imageSize, numClasses,'EncoderDepth',encoderDepth)
 
 %6.2 Display the network
-plot(lgraph)
+figure, plot(lgraph)
 
 %% 7. Train the U-net
 %7.1 Set training options (descent method, learning rate, epochs etc)
 options = trainingOptions('sgdm', ...
-    'InitialLearnRate',e-2, ...
-    'MaxEpochs',20, ...
+    'InitialLearnRate',1e-3, ...
+    'MaxEpochs',500, ...
     'MiniBatchSize', 20, ...
     'Plots','training-progress',...
     'VerboseFrequency',10);
@@ -69,7 +67,7 @@ options = trainingOptions('sgdm', ...
 % https://github.com/hongweilibran/wmh_ibbmTum - look for 
 
 %7.2 Train the network on the  dataset
-net = trainNetwork(auTrainingDS,lgraph,options)
+net = trainNetwork(trainingDS,lgraph,options)
 
 
 %% 7. Assess performance on training data
@@ -79,13 +77,15 @@ prediction = net.predict(imds);
 
 i=40;
 
-img = readimage(imds,i);
+trainingimage=readimage(imds,i);
 labelimage = uint16(readimage(labelds,i));
+
+predImage = prediction(:,:,1,i);
 
 figure
 title('Training dataset performance')
 subplot(2,2,1)
-imshow(img,[])
+imshow(trainingimage,[])
 title('Image')
 
 subplot(2,2,2)
@@ -93,9 +93,8 @@ imshow(labelimage,[])
 title('Label')
 
 subplot(2,2,3)
-imshow(prediction(:,:,1,i),[])
+imshow(predImage,[])
 title('Prediction')
-
 
 
 %% 8. Predict
